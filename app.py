5c59abb3a30cfd9ef6dc86953e3f2a2ec7752469
vivek-srini <38190456+vivek-srini@users.noreply.github.com>
2023-07-28 14:02:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -190,24 +190,13 @@ def remove_text(filename):
   return img,json_output
 
 
-report_text = st.text_input("Report Text")
 
-
-export_as_pdf = st.button("Export Report")
 
 def create_download_link(val, filename):
     b64 = base64.b64encode(val)  # val looks like b'...'
     return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
 
-if export_as_pdf:
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font('Arial', 'B', 16)
-    pdf.cell(40, 10, report_text)
-    
-    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
 
-    st.markdown(html, unsafe_allow_html=True)
 
 
 def translate_sentence(text, target_language='es'):
@@ -309,7 +298,9 @@ def main():
         with open("eng_output.pdf", "rb") as pdf_file:
             PDFbyte = pdf_file.read()
         
-        create_download_link(PDFbyte,"test")
+        download_url = create_download_link(PDFbyte, 'test')
+
+        st.markdown(download_url, unsafe_allow_html=True)
 
         # Display the generated images
         st.write("Generated Images:")
